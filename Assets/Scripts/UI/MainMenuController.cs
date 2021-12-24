3b43880fd0d2c80dc61128e0f9dc41d23a009580
Gabriel Jouet <gabriel.jouet@viacesi.fr>
2021-12-24 13:52:56 +0100

    Added a reset save button #55

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -7,6 +7,25 @@ using UnityEngine.SceneManagement;
 public class MainMenuController : MonoBehaviour
 {
     /// <summary>
+    /// Confirmation game object.
+    /// </summary>
+    [SerializeField]
+    private GameObject _firstConfirmation;
+
+    /// <summary>
+    /// Second confirmation game object.
+    /// </summary>
+    [SerializeField]
+    private GameObject _secondConfirmation;
+
+    /// <summary>
+    /// Done confirmation game object.
+    /// </summary>
+    [SerializeField]
+    private GameObject _doneConfirmation;
+
+
+    /// <summary>
     /// Method called to open level selection screen.
     /// </summary>
     public void LoadLevelSelection()
@@ -21,5 +40,34 @@ public class MainMenuController : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+
+    /// <summary>
+    /// Method called when we want to reset game save, this is the first confirmation.
+    /// </summary>
+    public void FirstReset()
+    {
+        _firstConfirmation.SetActive(true);
+    }
+
+
+    /// <summary>
+    /// Method called when we want to reset game save, this is the second confirmation.
+    /// </summary>
+    public void SecondReset()
+    {
+        _secondConfirmation.SetActive(true);
+    }
+
+
+    /// <summary>
+    /// Method called when we want to reset game save, this is the last confirmation.
+    /// </summary>
+    public void ResetSave()
+    {
+        Controller.Instance.SaveController.ResetData();
+
+        _doneConfirmation.SetActive(true);
     }
 }
