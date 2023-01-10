4d16a784e49a9d53e3319bc830a7f1cfc3a924c2
Gabriel Jouet <gabriel.jouet@viacesi.fr>
2023-01-10 16:45:48 +0100

    Fixed achievement display issue

--- Assets/Scripts/Achievements/AchievementMenu.cs
+++ Assets/Scripts/Achievements/AchievementMenu.cs
@@ -37,7 +37,7 @@ public class AchievementMenu : MonoBehaviour
         SaveFile saveFile = Controller.Instance.SaveController.SaveFile;
 
         for (int i = 0; i < achievements.Count; i ++)
-            Instantiate(_achievementPrefab, transform).Initialize(achievements[i], saveFile.Achievements.Contains(achievements[i].ID));
+            Instantiate(_achievementPrefab, transform).Initialize(achievements[i], !saveFile.Achievements.Contains(achievements[i].ID));
 
         _achievementSlider.maxValue = achievements.Count;
         _achievementSlider.value = saveFile.Achievements.Count;
